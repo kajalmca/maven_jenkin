3a51e1538e36e753c2eea4778fff2a5f38903b85
kajalmca <91126210+kajalmca@users.noreply.github.com>
2026-04-19 15:06:02 +0530

    Added22

--- MavenJenkin/src/test/java/demo/SampleTest.java
+++ MavenJenkin/src/test/java/demo/SampleTest.java
@@ -9,6 +9,9 @@ public class SampleTest {
 		@Test
 	public void case12(){
 		System.out.println("running this demo test12");
+			@Test
+	public void case22(){
+		System.out.println("running this demo test12");
 	}
 	
 
