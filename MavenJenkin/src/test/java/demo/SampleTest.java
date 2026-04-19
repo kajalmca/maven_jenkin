6fa007adf9109ae4fd2fa1e66f1589a54dd5d556
w3villa-kajal-yadav <115987859+w3villa-kajal-yadav@users.noreply.github.com>
2026-04-19 14:43:45 +0530

    Fisrt commit

---
+++ MavenJenkin/src/test/java/demo/SampleTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void case11(){
+		System.out.println("running this demo test");
+	}
+	
+
+}
---
+++ MavenJenkin/src/test/java/sample/DemoTest.java
@@ -0,0 +1,30 @@
+package sample;
+
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+	@Test
+	public void case1(){
+		System.out.println("1");
+	}
+
+
+@Test
+public void case2(){
+	System.out.println("2");
+}
+
+
+@Test
+public void case3(){
+	System.out.println("3");
+}
+
+
+@Test
+public void case4(){
+	System.out.println("4");
+}
+
+}
